a0b880d0a440c8d64c4e244217da31ae77c2df09
wowjd15978 <125331860+wowjd15978@users.noreply.github.com>
2023-02-28 17:18:33 +0900

    0228 카페

--- js/cafe.js
+++ js/cafe.js
@@ -30,11 +30,49 @@ $(function(){
     });
 
     //슬라이드 쇼
-    setInterval(mySlide, 1300);
+    let slide = setInterval(mySlide, 13000);
 
+    $('.next').click(function(){
+        clearInterval(slide);
+        mySlide();
+        slide();
+        setInterval(mySlide, 13000);
+    });
     
+    $('.prev').click(function(){
+        clearInterval(slide);
+        prevEvent();
+        setInterval(mySlide, 13000);
+    });
+    
+    mytime();
 
+
+    //데이터 가져오기
+
+    jQuery.ajax({
+        type:"GET",
+        url: "./date/data.json",
+        dataType: "JSON",
+        success: function(data){
+            let list = '';
+            for(let i = 0; i < data.cafelist.length; i++){
+                list +='<li><a herf=# class="d-flex align-items-center justify-content-between">';
+                list +='<div class="tbox d-flex align-items-center">';
+                list +='<img src="'+data.cafelist[i].img+'"alt="'+data.cafelist[i].num+'">';
+                list +='<h1>1</h1><p class="ellipise">'+data.cafelist[i].content+'</p></div>';
+                list +='<div class="cfe d-flex"><p class="ellipise">'+data.cafelist[i].cafename+'</p><p class="dg">'+data.cafelist[i].cafename+'</p></div>';
+                list += '</a></li>';
+            }
+            $('.clist').html(list);
+        },
+        error:function(xhr, status, error){
+            console.log(error);
+        } 
+    });
 }); //jqurey
+
+
 
 function mySlide(){
     const eq0 = $('.hero .new:eq(0)'); //이야기 하나
@@ -51,3 +89,22 @@ function mySlide(){
 function ranDomList(){
     return Math.floor(Math.random() * 4);
 };
+
+function prevEvent(){
+    $('.new:first-child').removeClass('zindex');
+    $('.new:last-child').addClass('zindex').clone().prependTo('.hero').end();
+    $('.new:last-child').remove();
+}
+
+function mytime(){
+    let dt = new Date();
+    let y = dt.getFullYear();
+    let m = dt.getMonth()+1;
+    let d = dt.getDay();
+    let h = dt.getHours();
+    let mm = dt.getMinutes();
+    
+    let mt = `${y}.${m}.${d}.<strong>${h}.${mm}</strong>`;
+
+    $('.thetime').html(mt);
+}
